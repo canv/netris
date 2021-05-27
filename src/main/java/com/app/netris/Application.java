414cd9fd550870f122ffe29bf3a9807d93c7abf6
canv <aleksandrinstall@gmail.com>
2021-05-27 10:29:34 +0300

    simple fix

--- src/main/java/com/app/netris/Application.java
+++ src/main/java/com/app/netris/Application.java
@@ -1,8 +1,5 @@
 package com.app.netris;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
